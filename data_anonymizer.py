eb2d9129a7cadb372c91ccdc8096f69cf3c0058a
kelvnt <btkelvin@gmail.com>
2020-09-05 00:12:18 +0800

    bug-fix: re-arranged order of regex and NER hashing for free text

--- data_anonymizer.py
+++ data_anonymizer.py
@@ -14,9 +14,9 @@ class DataAnonymizer:
     named entity recognition (NER) with a pretrained mdoel from the
     transformers package to pick up entities such as location and person, 
     generate a MD5 hash for the entity, replaces the entity with the hash,
-    and stores the hash to entity in a dictionary for de-anonymization. The 
-    same process is done for categorical columns, without the use of
-    NER. 
+    and stores the hash to entity in a dictionary for de-anonymization. A 
+    similar process is repeated for categorical columns, without the use
+    of NER.
     
     Please use at your own risk, NER identification is not perfect.
     
@@ -24,7 +24,17 @@ class DataAnonymizer:
     ----
     df: <pandas.DataFrame>
         dataframe of the data to be anonymized
+        
+    Methods
+    ----
+    anonymize:
+        anonymize the dataset with the specified free text / categorical
+        columns. returns an anonymized dataset and a hash dictionary
     
+    de_anonymize:
+        de-anonymize an anonymized dataset given the anonymized data and its
+        corresponding hash dictioanry. returns the de-anonymized version
+        of the dataset
     """
     def __init__(self, df):
         # type checking
@@ -78,14 +88,22 @@ class DataAnonymizer:
             dictionary with key as column name and value as a dictionary of
             hash key to the original data
         """
-        # arguments checking
-        assert isinstance(free_text_columns, (list, type(None))), "free_text_columns should be of type list"
-        assert isinstance(free_text_additional_regex_to_hash, (dict, type(None))), "free_text_additional_regex_to_hash should be of type dict"
-        assert isinstance(categorical_columns, (list, type(None))), "categorical_columns should be of type list"
-        assert isinstance(pretrained_model_name, str), "pretrained_model_name should be of type str"
-        assert isinstance(label_list, list), "label_list should be of type list"
-        assert isinstance(labels_to_anonymize, list), "labels_to_anonymize should be of type list"
-        assert set(labels_to_anonymize).issubset(set(label_list)), "elements in labels_to_anonymize should be in labels_list"
+        # type checking
+        assert isinstance(free_text_columns, (list, type(None))),\
+            "free_text_columns should be of type list"
+        assert isinstance(free_text_additional_regex_to_hash,
+                          (dict, type(None))),\
+            "free_text_additional_regex_to_hash should be of type dict"
+        assert isinstance(categorical_columns, (list, type(None))),\
+            "categorical_columns should be of type list"
+        assert isinstance(pretrained_model_name, str),\
+            "pretrained_model_name should be of type str"
+        assert isinstance(label_list, list),\
+            "label_list should be of type list"
+        assert isinstance(labels_to_anonymize, list),\
+            "labels_to_anonymize should be of type list"
+        assert set(labels_to_anonymize).issubset(set(label_list)),\
+            "elements in labels_to_anonymize should be in labels_list"
         
         self.label_list = label_list
         self.labels_to_anonymize = labels_to_anonymize
@@ -94,11 +112,13 @@ class DataAnonymizer:
         df_ = copy.deepcopy(self.df)
         hash_dict = {}
         
+        # hash the free text columns
         if free_text_columns is not None:
             _free_text_dict = {}
+            
             for col in free_text_columns:
                 
-                # check if there is any additional regex to hash for the col
+                # check for any specified additional regex to hash for the col
                 regex = None
                 if free_text_additional_regex_to_hash is not None:
                     if col in free_text_additional_regex_to_hash:
@@ -107,13 +127,17 @@ class DataAnonymizer:
                 df_[col], d_ = self._anonymize_free_text(df_[col].tolist(),
                                                          regex)
                 _free_text_dict.update({col: d_})
-            hash_dict.update({"free_text": _free_text_dict})
                 
+            hash_dict.update({"free_text": _free_text_dict})
+        
+        # hash the categorical columns
         if categorical_columns is not None:
             _categorical_dict = {}
+            
             for col in categorical_columns:
                 df_[col], d_ = self._anonymize_categorical(df_[col].tolist())
                 _categorical_dict.update({col: d_})
+                
             hash_dict.update({"categorical": _categorical_dict})
         
         return df_, hash_dict
@@ -130,8 +154,6 @@ class DataAnonymizer:
             list of regex patterns to hash
             
         Notes:
-            * might need to clean special characters
-            * how to ensure character separation is always a space
             * should save original word, not the token
             * how to de-tokenize berttokenizer?
         """
@@ -151,24 +173,14 @@ class DataAnonymizer:
         
         for row in l:
             # each s should not be longer than 512 characters
-            sentences = [s[:512] for s in row.split(".")]
+            sentences = [s for s in row.split(".")]
             anon_sentences = []
             
             for _sentence in sentences:
                 
-                # handle the regex portion
-                if additional_regex_to_hash is not None:
-                    for regex in additional_regex_to_hash:
-                        matches = re.findall(regex, _sentence)
-                        
-                        if matches:
-                            for _m in matches:
-                                _hash = hashlib.md5(str(_m).encode()).hexdigest()
-                                if _hash not in hash_dict_:
-                                    hash_dict_.update({_hash: _m})
-                                _sentence = _sentence.replace(_m, _hash)
-                                
-                # handle transformers NER recognition
+                #-----                
+                # NER Prediction & Hashing
+                #-----
                 
                 # tokenize sentence
                 tokens = tokenizer.tokenize(
@@ -182,42 +194,68 @@ class DataAnonymizer:
                 outputs = model(inputs)[0]
                 _preds = torch.argmax(outputs, dim=2)[0].tolist()
                 
-                # detokenize tokens
+                # detokenize tokens - need to detokenize better
                 # detokens = self._detokenize(tokens)
                 
-                # get words of entities to anonymize
+                #----
+                # Regex Hashing
+                #----
+                # hash regex before NER hashing, but after NER tokenization to
+                # prevent (i) regex picking up NER hash values and (ii) NER
+                # tokenizing regex hash values
+                if additional_regex_to_hash is not None:
+                    for regex in additional_regex_to_hash:
+                        matches = re.findall(regex, _sentence)
+                        
+                        if matches:
+                            for _m in matches:
+                                _hash = (hashlib.md5(str(_m).encode())
+                                         .hexdigest())
+                                if _hash not in hash_dict_:
+                                    hash_dict_.update({_hash: _m})
+                                _sentence = _sentence.replace(_m, _hash)
+                
+                #----
+                # End Regex Hashing
+                #----
+                
+                #----
+                # Continue NER Prediction & Hashing
+                #----
                 words_to_anonymize = []
                 
+                # loop over every token prediction
                 for i, pred in enumerate(_preds):
+                    # get prediction label
                     label = label_list[pred]
                     
+                    # check if label should be anonymized
                     if label in labels_to_anonymize:
+                        # get original word
                         word = tokens[i].replace("##", "") # detokens[i]
                         
-                        if len(words_to_anonymize) == 0:
-                            words_to_anonymize.append(word)
-                            prev_i = i
-                            prev_label = label
-                            
-                        elif len(words_to_anonymize) > 0:
-                            if (i-1 == prev_i) & (label == 
-                                                  prev_label):
+                        # if consecutive tokens with same label, concat tokens
+                        # else append token
+                        if len(words_to_anonymize) > 0:
+                            if ((i-1 == prev_i) & (label == prev_label)):
                                 words_to_anonymize[-1] = (
                                     words_to_anonymize[-1] + " " + word 
                                 )
                                 prev_i = i
                                 prev_label = label
-                                
                             else:
                                 words_to_anonymize.append(word)
                                 prev_i = i
                                 prev_label = label
+                        else:
+                            words_to_anonymize.append(word)
+                            prev_i = i
+                            prev_label = label
                 
                 for _word in words_to_anonymize:
                     _hash = hashlib.md5(str(_word).encode()).hexdigest()
                     if _hash not in hash_dict_:
                         hash_dict_.update({_hash: _word})
-                        
                     _sentence = _sentence.replace(_word, _hash)
                 
                 anon_sentences.append(_sentence)
@@ -230,7 +268,9 @@ class DataAnonymizer:
     
     def _detokenize(self, tokens):
         """
-        to-do: lengths does not match..
+        to-do:
+            * lengths does not match?
+            * no proper way to detokenize
         """
         
         is_subtoken = lambda x: True if x[:2] == "##" else False
@@ -285,11 +325,13 @@ class DataAnonymizer:
         
         df_ = copy.deepcopy(anonymized_df)
         
+        # de_anonymize free text columns
         if "free_text" in hash_dictionary:
             for col, _hash_dict in hash_dictionary["free_text"].items():
                 for _hash, _original in _hash_dict.items():
                     df_[col] = df_[col].str.replace(_hash, _original)
-                    
+        
+        # de_anonymize categorical columns
         if "categorical" in hash_dictionary:
             for col, _hash_dict in hash_dictionary["categorical"].items():
                 df_[col] = df_[col].replace(_hash_dict)
