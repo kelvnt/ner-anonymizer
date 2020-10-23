de8df3186cbae5efc316887f40f12e14e0d0e298
kelvnt <btkelvin@gmail.com>
2020-10-23 17:38:34 +0800

    v0.1.5 from bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ner-anonymizer",
-    version="0.1.4",
+    version="0.1.5",
     author="Kelvin Tay",
     author_email="btkelvin@gmail.com",
     description="Anonymizes pandas dataset and provides a hash dictionary to de-anonymize",
